9cdf528c4ebe903b4f1f954dfbe73ec4a8a751de
otabekkhabibullaev <otabek.khabibullaev@alif.uz>
2025-02-03 13:58:54 +0500

    String Concatenation

--- index.php
+++ index.php
@@ -1,44 +1,31 @@
 <?php
+    $firstName = 'Otabek';
+    $lastNane = 'Khabibullaev';
+    $fullName = $firstName . ' ' . $lastNane;
+?>
 
-// string
-$name = 'Otabek';
-$name2 = 'Aziz';
-var_dump($name);
-echo '<br>';
-
-// integer
-$age = 22;
-var_dump($age);
-echo '<br>';
-
-// float
-$rating = 4.5;
-var_dump($rating);
-echo '<br>';
-
-// boolean
-$isLoading = true;
-var_dump($isLoading);
-echo '<br>';
-
-// Array
-$array = [1,2,3,4,5,6];
-var_dump($array);
-echo '<br>';
-
-// Object
-$person = new stdClass();
-$person->name = 'Otabek';
-$person->age = 20;
-$person->rating = 5;
-var_dump($person);
-echo '<br>';
-
-// Null
-$null = null;
-var_dump($null);
-echo '<br>';
-
-// Resource
-//$file = fopen('data.txt', 'r');
-//var_dump($file);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Learn PHP From Scratch</title>
+</head>
+<body class="bg-gray-100">
+<header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+        <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+    </div>
+</header>
+<div class="container mx-auto p-4 mt-4">
+    <div class="bg-white rounded-lg shadow-md p-6">
+         <!-- output -->
+        <?= $fullName  ?>
+        <h2><?= "Hello, my name is $firstName $lastNane" ?></h2>
+    </div>
+</div>
+</body>
+</html>
