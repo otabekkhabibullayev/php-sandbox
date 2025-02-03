d6f88072b872a076b222b0de7cb5fed37e6ad610
otabekkhabibullaev <otabek.khabibullaev@alif.uz>
2025-02-03 10:43:58 +0500

    Variables

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+    $title = 'Learn PHP From Scratch';
+    $heading = 'Welcome To The Course';
+    $body = 'In this course, you will learn the fundamentals of php';
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,7 +12,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title><?php echo 'Learn PHP From Scratch'; ?></title>
+    <title><?= $title  ?></title>
 </head>
     <body class="bg-gray-100">
         <header class="bg-blue-500 text-white p-4">
@@ -16,8 +22,8 @@
         </header>
         <div class="container mx-auto p-4 mt-4">
             <div class="bg-white rounded-lg shadow-md p-6">
-                <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Course' ?></h2>
-                <?php echo '<p>In this course, you will learn the fundamentals of php</p>' ?>
+                <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
+                <p><?= $body ?></p>
             </div>
         </div>
     </body>
