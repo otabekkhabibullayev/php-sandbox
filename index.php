c13baccd0e505e3b5054cc3cfe10b27ddf6632bd
otabekkhabibullaev <otabek.khabibullaev@alif.uz>
2025-02-03 10:34:13 +0500

    PHP Tags, Printing & Comments

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title><?php echo 'Learn PHP From Scratch'; ?></title>
+</head>
+    <body class="bg-gray-100">
+        <header class="bg-blue-500 text-white p-4">
+            <div class="container mx-auto">
+                 <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch' ?></h1>
+            </div>
+        </header>
+        <div class="container mx-auto p-4 mt-4">
+            <div class="bg-white rounded-lg shadow-md p-6">
+                <h2 class="text-2xl font-semibold mb-4"><?= 'Welcome To The Course' ?></h2>
+                <?php echo '<p>In this course, you will learn the fundamentals of php</p>' ?>
+            </div>
+        </div>
+    </body>
+</html>
