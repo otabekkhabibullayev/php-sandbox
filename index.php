b5b93e39017f85602d63c4601fdc3dce4594bf96
otabekkhabibullaev <otabek.khabibullaev@alif.uz>
2025-02-03 11:22:33 +0500

    Data types

--- index.php
+++ index.php
@@ -1,30 +1,44 @@
 <?php
-    $title = 'Learn PHP From Scratch';
-    $heading = 'Welcome To The Course';
-    $body = 'In this course, you will learn the fundamentals of php';
-?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title><?= $title  ?></title>
-</head>
-    <body class="bg-gray-100">
-        <header class="bg-blue-500 text-white p-4">
-            <div class="container mx-auto">
-                 <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch' ?></h1>
-            </div>
-        </header>
-        <div class="container mx-auto p-4 mt-4">
-            <div class="bg-white rounded-lg shadow-md p-6">
-                <h2 class="text-2xl font-semibold mb-4"><?= $heading ?></h2>
-                <p><?= $body ?></p>
-            </div>
-        </div>
-    </body>
-</html>
+// string
+$name = 'Otabek';
+$name2 = 'Aziz';
+var_dump($name);
+echo '<br>';
+
+// integer
+$age = 22;
+var_dump($age);
+echo '<br>';
+
+// float
+$rating = 4.5;
+var_dump($rating);
+echo '<br>';
+
+// boolean
+$isLoading = true;
+var_dump($isLoading);
+echo '<br>';
+
+// Array
+$array = [1,2,3,4,5,6];
+var_dump($array);
+echo '<br>';
+
+// Object
+$person = new stdClass();
+$person->name = 'Otabek';
+$person->age = 20;
+$person->rating = 5;
+var_dump($person);
+echo '<br>';
+
+// Null
+$null = null;
+var_dump($null);
+echo '<br>';
+
+// Resource
+//$file = fopen('data.txt', 'r');
+//var_dump($file);
