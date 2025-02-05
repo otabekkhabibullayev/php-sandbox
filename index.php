730969368487f7a12aa993ec442058b0866b61d7
otabekkhabibullaev <otabek.khabibullaev@alif.uz>
2025-02-05 11:34:03 +0500

    Arithmetic Operators & Functions

--- index.php
+++ index.php
@@ -1,7 +1,55 @@
 <?php
-    $firstName = 'Otabek';
-    $lastNane = 'Khabibullaev';
-    $fullName = $firstName . ' ' . $lastNane;
+    $output = null;
+
+    $num1 = 10;
+    $num2 = 20;
+
+    // Basic Math
+    $output = "$num1 + $num2 = " . ($num1 + $num2);
+    $output = "$num1 - $num2 = " . ($num1 - $num2);
+    $output = "$num1 * $num2 = " . ($num1 * $num2);
+    $output = "$num1 / $num2 = " . ($num1 / $num2);
+    $output = "$num1 % $num2 = " . ($num1 % $num2);
+
+    //Assignment operator
+
+    $num3 = 10;
+
+//    $output = $num3 += 20;
+//    $output = $num3 -= 20;
+//    $output = $num3 *= 20;
+    $output = $num3 /= 2;
+
+    // Built-in php functions
+
+    // rand()
+    $output = rand();
+    $output = getrandmax(); // max rand value
+    $output = rand(1, 100);
+
+    //round()
+    $output = round(2.5);
+
+    // ceil()
+    $output = ceil(1.1);
+
+    //floor
+    $output = floor(1.9);
+
+    // sqrt()
+    $output = sqrt(121);
+
+    //pi()
+    $output =  pi();
+
+    //abs()
+    $output = abs(1.9);
+
+    //max()
+    $output = max(1,2,3,40,5);
+
+    //min()
+    $output = min(1,2,3,40,5);
 ?>
 
 <!doctype html>
@@ -23,8 +71,7 @@
 <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6">
          <!-- output -->
-        <?= $fullName  ?>
-        <h2><?= "Hello, my name is $firstName $lastNane" ?></h2>
+        <?= $output  ?>
     </div>
 </div>
 </body>
